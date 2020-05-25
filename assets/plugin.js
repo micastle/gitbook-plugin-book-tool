335559edc3fbb87881845aa0d7ca2f3aed413918
Clark <micastlecl@hotmail.com>
2020-05-25 17:13:44 +0800

    init code

---
+++ assets/plugin.js
@@ -0,0 +1,23 @@
+var gitbook = window.gitbook;
+
+gitbook.events.on('page.change', function() {
+	
+	var book_tool_button = ['<div class="book-tool"><i class="fa fa-arrow-up"></i></div>'].join("");
+	$(".book").append(book_tool_button)
+
+	$(".book-tool").hide();
+
+	$('.book-body,.body-inner').on('scroll', function () {
+		if ($(this).scrollTop() > 100) { 
+			$('.book-tool').fadeIn();
+		} else {
+			$('.book-tool').fadeOut();
+		}
+	});
+
+	$('.book-tool').click(function () { 
+		window.alert('show comment panel on click');
+		return false;
+	});
+	
+});
